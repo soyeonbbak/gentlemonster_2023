810d57a2919b23cd532a8a4b2b458acce33ce516
soyeon <illil@naver.com>
2023-08-09 18:10:34 +0900

    로그인

--- script/login.js
+++ script/login.js
@@ -1,3 +1,5 @@
 const loginTitleA = document.querySelectorAll('.login_title a')
-const loginContents = document.querySelectorAll('.login_content > div')
+const loginContents = document.querySelectorAll('.login_contents > div')
 console.log(loginTitleA,loginContents)
+loginContents[1].style.display ='none'
+loginContents[2].style.display ='none'
